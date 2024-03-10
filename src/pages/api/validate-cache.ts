0a9282beeb2744f6c331172c9f29cf3161cbcb98
Waleed Salama <waleed@salama.ws>
2024-03-10 23:36:27 +0300

    Using edge runtime for validate-cache api

--- src/pages/api/validate-cache.ts
+++ src/pages/api/validate-cache.ts
@@ -7,6 +7,9 @@ import {
   type CacheValidationResponseData,
 } from "~/lib/api-types";
 
+// To use edge runtime on Vercel
+export const runtime = "edge";
+
 // to limit concurrency with promises
 import pLimit from "p-limit";
 const limit = pLimit(5);
